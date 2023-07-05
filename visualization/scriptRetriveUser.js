0e928443f111130901cd56ec8ebe88418691cbd4
magicWiss <mat.wissel@stud.uniroma3.it>
2023-07-05 11:51:04 +0200

    fix dei dataset. Creazione dei dataset sample per la visualizzazione

--- visualization/scriptRetriveUser.js
+++ visualization/scriptRetriveUser.js
@@ -1,25 +1,51 @@
+
+
 // Load the CSV file
 var specificID="4782551"
-d3.csv("user2date2setimentAGG.csv").then(function(data) {
-    // Specify the specific ID to filter
+function print_data_user_hastag(data)
+{
+  var container = d3.select('#users');
+                
+                // Iterate over the JSON data and create elements
+                for (i in data) 
+                {
+                  total=data[i]['total']
+                  id=i
+                  container.append('p').text(id +" " + total);
+                }
 
+}
+function  print_data_word_cloud(data)
+{
   
-    // Filter rows based on specific ID
-    var filteredData = data.filter(function(d) {
-      return d.author_id === specificID;
-    });
-  
-    // Display the filtered data
-    var outputDiv = d3.select("#output");
-    outputDiv.selectAll("p")
-      .data(filteredData)
-      .enter()
-      .append("p")
-      .text(function(d) {
-        return JSON.stringify(d);
-      });
-  });
+  var container = d3.select('#wordcloud');
+                
+                // Iterate over the JSON data and create elements
+                for (i in data) 
+                {
+                  total=data[i]['total']
+                  id=i
+                  container.append('p').text(id +" " + total);
+                }
+                  
+}
+Promise.all([
+  d3.csv("Sample\\user2date2setimentAGG.csv"),
+  d3.json("Sample\\sampleWordCloud.json"),
+  d3.csv("Sample\\hastag2date2setiment.csv"),
+  d3.json("Sample\\user2hastag.json")
 
+]).then(function(files) {
+  wordCloud=files[1]
+  hash2date=files[2]
+  user2hashtag=files[3]
+  user2date=files[0]
+  print_data_word_cloud(wordCloud)
+  print_data_user_hastag(user2hashtag)
+
+}).catch(function(err) {
+ 
+})
   
   
   
